eeb20a0b567583087ff68048a4ce66271a365902
MagdaLem <mlike@live.de>
2025-01-28 15:15:09 +0100

    update service class

--- src/main/java/com/malem/listwave/service/ToDoListService.java
+++ src/main/java/com/malem/listwave/service/ToDoListService.java
@@ -8,53 +8,62 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
-public class ToDoListService
-{
-    private final Map<Long,Task> toDoList = new HashMap<>();
+public class ToDoListService {
+    private final Map<Long, Task> toDoList = new HashMap<>();
     private final AtomicLong nextId = new AtomicLong(1);
 
-    public Task createTask(Task task){
+    public Task createTask(Task task) {
 
         Long id = nextId.getAndIncrement();
-
         task.setId(id);
-        toDoList.put(id,task);
+        toDoList.put(id, task);
 
         return task;
     }
 
-    public Task updateTask(Long id, Task updatetask){
+    public Task updateTask(Long id, Task updatetask) {
 
         Task currentTask = toDoList.get(id);
 
-        if (currentTask != null){
+        if (currentTask != null) {
             currentTask.setDescription(updatetask.getDescription());
             currentTask.setStatus(updatetask.getStatus());
-            toDoList.put(id,currentTask);
+            toDoList.put(id, currentTask);
         }
 
         return currentTask;
     }
 
-    public boolean deleteTask(Long id){
-        return toDoList.remove(id) != null;
+    public boolean deleteTask(Long id) {
+
+        if (getTask(id) != null) {
+            toDoList.remove(id);
+            return true;
+        }
+        return false;
     }
 
-    public boolean deleteAllTasks()
-    {
+    public boolean deleteAllTasks() {
         toDoList.clear();
 
         return toDoList.isEmpty();
     }
 
-    public List<Task> getToDoList(){
-        return new ArrayList<>(toDoList.values());
+    public List<Task> getToDoList() {
+
+        List<Task> tasks = new ArrayList<>();
+        for (Task t : toDoList.values()) {
+            if (t != null) {
+                tasks.add(t);
+            }
+        }
+        return tasks;
+
     }
 
-    public Task getTask(Long id){
+    public Task getTask(Long id) {
         return toDoList.get(id);
     }
-
 
 
 }
